1bdcf10a5d743978cfc45e71697f0cd3e66a5eca
joelbiju547 <joelbiju547@gmail.com>
2025-04-26 17:48:07 +0000

    Revert "firebase initialised"
    This reverts commit 3dcceaef6c188e6c9db5a1bf468e68fe49f3b80a.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:myapp/firebase_options.dart';
 import 'package:myapp/screens/signup_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
 
