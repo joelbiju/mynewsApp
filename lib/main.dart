f797fd02e74fcfe6923f72870d773fdc289d04ff
joelbiju547 <joelbiju547@gmail.com>
2025-05-01 11:55:00 +0000

    apk added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/firebase_options.dart';
-import 'package:myapp/screens/create_news_screen.dart';
 import 'package:myapp/screens/signup_screen.dart';
 
 Future<void> main() async {
